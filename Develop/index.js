3cef5cbcd778c2b9e3a7633ef646bb6f19a4eca3
Brett Miller <brett.miller47@yahoo.com>
2022-09-07 20:38:54 -0400

    pseudocoded the choice.menu if statements for updating sql db

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const inquirer = require('inquirer');
 
+// const db = require('./db/db.')
+
 const PORT = process.env.port || 3001;
 const app = express();
 
@@ -14,17 +16,19 @@ inquirer
     }
   ]).then((choice) => {
     if (choice.menu == 'View All Employees') {
-      
+      // db.query('SELECT employee FROM employees;');
     } else if (choice.menu == 'Add Employee') {
-
+      // db.query(`INSERT INTO employee(id, name, etc.) VALUES(${id}, ${name}, etc.);`);
     } else if (choice.menu == 'Update Employee Role') {
-
+      // db.query(`;`);
     } else if (choice.menu == 'View All Roles') {
-
+      // db.query('SELECT role FROM roles GROUP BY role;');
     } else if (choice.menu == 'Add Role') {
-      
+      // inqurer.prompt([{type: 'input', message: 'Enter role', name: 'role'}]).then((answer) =>
+      //  db.query(`INSERT INTO roles(role) VALUES(${answer.role});`)
+      // )
     } else if (choice.menu == 'View All Departments') {
-      
+      // db.query('SELECT department FROM departments GROUP BY department;');      
     } else if (choice.menu == 'Add Department') {
       
     }
